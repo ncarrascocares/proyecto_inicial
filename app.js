530d4141598ccdf5301d276c3188e9e9929e895d
ncarrascocares <nicolascarrasco15@gmail.com>
2025-01-20 02:14:02 -0300

    Se agrega textoa al index.html utilizando el archivo js y usuando los tag h1 y p del index

---
+++ app.js
@@ -0,0 +1,6 @@
+let titulo = document.querySelector('h1');
+let parrafo = document.querySelector('p');
+
+// Agregando texto a los tag declarados
+titulo.innerHTML = 'Juego del número secreto';
+parrafo.innerHTML = 'Ingresa un número del 1 al 10';
